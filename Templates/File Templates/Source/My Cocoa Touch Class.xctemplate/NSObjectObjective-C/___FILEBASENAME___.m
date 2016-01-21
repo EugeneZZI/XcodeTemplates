72e14d6de323e5ef93ab62db7d0e70949f8c5673
ZZI <LegionZZI@gmail.com>
2016-01-21 14:49:55 +0200

    Added NSObject templates

---
+++ Templates/File Templates/Source/My Cocoa Touch Class.xctemplate/NSObjectObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - Public Methods
+
+#pragma mark - Private Methods
+
+@end
---
+++ Templates/File Templates/Source/My Cocoa Touch Class.xctemplate/NSObjectSwift/___FILEBASENAME___.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+    
+    // MARK: - Properties
+        
+    // MARK: - Public Methods
+    
+    // MARK: - Private Methods
+    
+}
